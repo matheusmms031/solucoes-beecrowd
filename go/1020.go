38a8d46adaacbf04249cbe5a588007d053166390
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-01-24 09:02:51 -0300

    Create 1020.go

---
+++ go/1020.go
@@ -0,0 +1,14 @@
+package main
+ 
+import (
+    "fmt"
+)
+ 
+func main() {
+    var n1 int
+    fmt.Scan(&n1)
+	  years := n1 / 365
+    weaks := (n1 - (years*365))/30
+    days := n1 - ((years*365) + weaks*30)
+    fmt.Printf("%v ano(s)\n%v mes(es)\n%v dia(s)\n",years,weaks,days)
+}
