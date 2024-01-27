dc6c1b35e6abde92e19ff22d67c1ac8f25a68ad4
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-01-27 10:15:05 -0300

    Create 1041.java

---
+++ java/1041.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class 1041 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        String[] entrada = scan.nextLine().split(" ");
+        float x = Float.parseFloat(entrada[0]);
+        float y = Float.parseFloat(entrada[1]);
+        if(x > 0){
+            if(y < 0){
+                System.out.println("Q4");
+            }
+            if(y > 0){
+                System.out.println("Q1");
+            }
+            if(y == 0){
+                System.out.println("Eixo X");
+            }
+
+        }
+        if(x < 0){
+            if(y < 0){
+                System.out.println("Q3");
+            }
+            if(y > 0){
+                System.out.println("Q2");
+            }
+            if(y == 0){
+                System.out.println("Eixo X");
+            }
+
+        }
+        if(x == 0){
+            if(y != 0){
+                System.out.println("Eixo Y");
+            }
+            else{
+                System.out.println("Origem");
+            }
+
+        }
+    }
+
+}
