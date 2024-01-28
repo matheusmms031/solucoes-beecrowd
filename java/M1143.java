5c6fa2a7d878625435a84eb434354a068f7083e3
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-01-28 15:56:39 -0300

    Update

--- java/M1143.java
+++ java/M1143.java
@@ -2,14 +2,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 
-public class Main {
+public class M1143 {
 
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         int n1 = scan.nextInt();
         for(int c = 1; c <= n1; c++){
             System.out.printf("%d %d %d\n",c,c*c,c*c*c);
-            System.out.printf("%d %d %d\n",c,c*c+1,c*c*c+1);
         }
     }
 
