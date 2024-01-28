e4e001b7d77325a7f1fa44480d4e3b742da4cd8b
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-01-28 15:07:47 -0300

    Create 1096.java

---
+++ java/1096.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+
+
+public class 1096 {
+
+    public static void main(String[] args) throws IOException {
+        for(int n1 = 1; n1 <= 9; n1++){
+            for(int n2 = 7; n2 >= 5; n2--){
+                System.out.printf("I=%d J=%d\n",n1,n2);
+            }
+        }
+    }
+
+}
+
+
