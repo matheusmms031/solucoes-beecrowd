266ded7adec077b4a56125f56467ced359f9df74
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-01-28 15:14:41 -0300

    Create 1097.java

---
+++ java/1097.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+
+
+public class 1097 {
+
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        for(int n1 = 1; n1 <= 9; n1+= 2){
+            for(int n2 = 7+count; n2 >= 5+count; n2--){
+                System.out.printf("I=%d J=%d\n",n1,n2);
+            }
+            count += 2;
+        }
+    }
+
+}
