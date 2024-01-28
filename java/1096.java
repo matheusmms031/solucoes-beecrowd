27384945f1c79d885083ab4917aca23ff0c5cfb7
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-01-28 15:10:34 -0300

    Update 1096.java

--- java/1096.java
+++ java/1096.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class 1096 {
 
     public static void main(String[] args) throws IOException {
-        for(int n1 = 1; n1 <= 9; n1++){
+        for(int n1 = 1; n1 <= 9; n1+= 2){
             for(int n2 = 7; n2 >= 5; n2--){
                 System.out.printf("I=%d J=%d\n",n1,n2);
             }
@@ -13,5 +13,3 @@ public class 1096 {
     }
 
 }
-
-
