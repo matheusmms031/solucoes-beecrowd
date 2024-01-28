8f7f34db09e0d8c5a4893d1d847e1d29b02a7c41
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-01-28 15:51:36 -0300

    Create 1144.java

---
+++ java/1144.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+        for(int c = 1; c <= n1; c++){
+            System.out.printf("%d %d %d\n",c,c*c,c*c*c);
+            System.out.printf("%d %d %d\n",c,c*c+1,c*c*c+1);
+        }
+    }
+
+}
+
+
+
+
+
