6331a5a527d089029fb8d1208a60e8a3d1f4c32b
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-01-28 15:28:37 -0300

    Create 1133.java

---
+++ java/1133.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class 1133 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+        int maior;
+        int menor;
+        if(n1 > n2) {
+            maior = n1;
+            menor = n2;
+        }else{
+            maior = n2;
+            menor = n1;
+        }
+
+        for(int c = menor; c < maior; c++){
+            if(c % 5 == 2 || c % 5 == 3){
+                System.out.println(c);
+            }
+        }
+    }
+
+}
