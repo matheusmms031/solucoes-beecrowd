af925b81100150a058ed0fb8f8617e64b1a553cf
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-01-28 15:03:32 -0300

    Create 1095.java

---
+++ java/1095.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+
+
+public class 1095 {
+
+    public static void main(String[] args) throws IOException {
+        int n2 = 1;
+        for(int n1 = 60; n1 >= 0; n1 -= 5){
+            System.out.printf("I=%d J=%d\n",n2,n1);
+            n2 += 3;
+        }
+    }
+
+}
