affcd82d110f0109b87cad6205f19f277d134d24
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-01-28 15:41:25 -0300

    Create 1132.java

---
+++ java/1132.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class 1132 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+        int soma = 0;
+        int maior, menor;
+        if(n1 > n2) {
+            maior = n1;
+            menor = n2;
+        }else{
+            maior = n2;
+            menor = n1;
+        }
+
+        for(int c = menor; c <= maior; c++){
+            if(c % 13 != 0){
+                soma += c;
+            }
+        }
+        System.out.println(soma);
+    }
+
+}
